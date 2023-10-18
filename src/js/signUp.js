50beadf0a246daf6389cbdc0dc1471a2ce1fb8f4
CroniX-Business <petarruskan@gmail.com>
2023-10-18 13:23:56 +0200

    Updated signUp

---
+++ src/js/signUp.js
@@ -0,0 +1,34 @@
+function showPassword() {
+  var checkbox = document.getElementById('toggle');
+  var label = document.getElementById('toggleLabel');
+  var passwordInput = document.getElementById('password');
+  var repeatPasswordInput = document.getElementById('repeat-password');
+
+
+  if (checkbox.checked) {
+    label.innerText = 'hide';
+    passwordInput.type = 'text';
+    repeatPasswordInput.type = 'text';
+  } else {
+    label.innerText = 'show';
+    passwordInput.type = 'password';
+    repeatPasswordInput.type = 'password';
+  }
+}
+
+function openPopup() {
+  var popup = document.getElementById('popup');
+  var overlay = document.getElementById('overlay');
+
+  popup.style.display = 'block';
+  overlay.style.display = 'block';
+}
+
+function closePopup() {
+  var popup = document.getElementById('popup');
+  var overlay = document.getElementById('overlay');
+
+  popup.style.display = 'none';
+  overlay.style.display = 'none';
+}
+
