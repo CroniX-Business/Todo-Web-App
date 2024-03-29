bdf6222c645fcc72a37adec8f5719d3e2d830602
Pruskan <petarruskan@gmail.com>
2024-03-29 19:53:03 +0100

    Change DB connection to use secret variable form .env

--- server/database/database.js
+++ server/database/database.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect('mongodb+srv://petarruskan:wvUrxbhMBuoGeXgD@profiles.jkhnkcr.mongodb.net/todo', {
+    const connection = await mongoose.connect(process.env.DATABASE, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
